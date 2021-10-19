8650c01582607201e6f27cbedfbb15f64187ae3c
Max <m.giller.dev@gmail.com>
2021-10-19 22:10:54 +0200

    Implemented home assistant mqtt binder

--- src/homeassistantcounter.py
+++ src/homeassistantcounter.py
@@ -1,17 +1,24 @@
 from peoplecounter import PeopleCounter
 from sensor.vl53l1xsensor import VL53L1XSensor
 import paho.mqtt.client as mqtt
+from HaMqtt.MQTTSensor import MQTTSensor
+import logging
 
 
 HA_URL = ""
 HA_PORT = 1883
-HA_TOPIC = ""
+HA_SENSOR_NAME = ""
+HA_SENSOR_ID = ""
 
 
 # Setup connection to HA
 mqttClient = mqtt.Client()
 mqttClient.connect(HA_URL, HA_PORT)
 mqttClient.loop_start()  # Keep conneciton alive
+
+# Setup mqtt binding
+sensor = MQTTSensor(HA_SENSOR_NAME, HA_SENSOR_ID, mqttClient)
+logging.debug(f'Connected to topic {sensor.state_topic}')
 
 
 def countChange(change: int) -> None:
@@ -22,7 +29,10 @@ def countChange(change: int) -> None:
         change (int): Number of people leaving (<0) or entering (>0) a room.
     """
     # Send update to HA
-    mqttClient.publish(HA_TOPIC, change)
+    global sensor
+    sensor.publish_state(change)
+    
+    logging.debug(f'People count changed by {change}')
 
 
 # Setup people count sensor
